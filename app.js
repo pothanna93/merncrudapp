754a96582cbe371808fa89d5ceafdfd76f6ec2c9
pothanna <pothanna.pullola@gmail.com>
2023-03-18 10:22:58 +0530

    mern crud backend changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//require("dotenv").config();
-require("dotenv-extended").load();
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 require("./Db/connection");
